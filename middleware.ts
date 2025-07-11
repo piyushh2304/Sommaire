9e3112f40935fc8625edde656e605a178f14b1be
Piyush Rajput <118372177+piyushh2304@users.noreply.github.com>
2025-07-11 11:29:51 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,22 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
-  "/summaries(.*)",
   "/upload(.*)",
+  "/summaries(.*)",
+  "/summary(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
+  // No return needed for public routes
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+   matcher: [
+    // Exclude _next, static files, and files with extensions
+    '/((?!_next|.*\\..*).*)',
+    '/(api|trpc)(.*)',
   ],
 };
